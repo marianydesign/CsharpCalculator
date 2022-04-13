d238de3f09b1e00745ac6895ed511f0b839c9922
marianydesign <info@marianydesign.com>
2022-04-13 12:40:20 +0200

    Second upload

--- My_Calculator/Properties/AssemblyInfo.cs
+++ My_Calculator/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CsharpCalculator")]
-[assembly: AssemblyCopyright("Copyright © 2022")]
+[assembly: AssemblyCopyright("Copyright © 2022 MARIANYDESIGN")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
